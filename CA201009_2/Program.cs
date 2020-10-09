3af6f689e2874d5cfffe36d6a3c3e47b980b4065
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2020-10-09 12:03:15 +0200

    Add project files.

---
+++ CA201009_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA201009_2
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+
+        private static Random rnd = new Random();
+        private static string GetRndPassword(int length)
+        {
+
+            return null;
+        }
+    }
+}
